7f3559556e108550802aa01bb01ffa8fa3b04ea4
lancewburklin <1677@holbertonschool.com>
2020-08-17 20:36:16 +0000

    Makes a directory and tgz file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This file will help set up the web static stuff with a .tgz archive
+"""
+
+def do_pack():
+    """ This sets up the tgz """
+    from datetime import datetime
+    from fabric.api import local
+    time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    local('mkdir -p versions')
+    try:
+        local('tar -cvzf versions/web_static_{} /web_static'.format(time))
+    except:
+        return None
