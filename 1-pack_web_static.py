6b2faa4c75a4c67f252d061ea7deba5c5c0bbc02
lancewburklin <1677@holbertonschool.com>
2020-08-17 20:50:02 +0000

    Fixed datetime issue and archive issues

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,9 @@ def do_pack():
     """ This sets up the tgz """
     from datetime import datetime
     from fabric.api import local
-    time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
     local('mkdir -p versions')
     try:
-        local('tar -cvzf versions/web_static_{} /web_static'.format(time))
+        local('tar -cvzf versions/web_static_{}.tgz web_static'.format(time))
     except:
         return None
