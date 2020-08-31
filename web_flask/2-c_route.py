be32f4762d4a7cf094eb01298d455f3cd236197a
lancewburklin <1677@holbertonschool.com>
2020-08-31 20:32:31 +0000

    Adding the custom text route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Here we are again, with more Flask stuff
+"""
+from web_flask import app
+
+
+@app.route('/', strict_slashes=False)
+def chicken():
+    """ More chicken stuff I guess """
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def pineapples():
+    """ Pineapples are cool """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cheese(text=""):
+    """ No cheese for sure """
+    return 'C ' + text
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
