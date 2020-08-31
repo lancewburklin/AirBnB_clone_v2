eeabf2d3fd3a55034a9a51c4633877cdeadb9f32
lancewburklin <1677@holbertonschool.com>
2020-08-31 17:40:00 +0000

    FIrst Flask script

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+First Flask script
+"""
+
+
+from web_flask import app
+
+@app.route('/', strict_slashes=False)
+def chicken():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
